5f72cacc2f6ac01f505ab19f325fe04d090fa962
PedroDP1804 <pedropaulacampos@gmail.com>
2024-07-08 17:48:13 -0300

    Doxygen 1

--- dominios.h
+++ dominios.h
@@ -9,6 +9,10 @@
 
 using namespace std;
 
+/**
+ * Senha no formato XXXXXX, em que é um dígito de 0-9. Não pode haver dígitos duplicados e o primeiro dígito deve ser diferente de 0.
+ * A senha não pode ser uma sequência crescente ou decresente de dígitos.
+ */
 //Matricula: 170153169
 class Senha {
     private:
@@ -16,10 +20,25 @@ class Senha {
         void validar(const std::string&);
 
     public:
+        /**
+         * @brief Atualiza o valor da Senha.\n
+         * Senha no formato XXXXXX, em que é um dígito de 0-9. Não pode haver dígitos duplicados e o primeiro dígito deve ser diferente de 0.
+         * A senha não pode ser uma sequência crescente ou decresente de dígitos.
+         * @param valor (string).
+         * @throw Lança invalid_argument caso valor seja inválido.
+         */
         void setValor(const std::string&);
+        /**
+         * @brief Retorna o valor atual da Senha.
+         * @return valor (string).
+         */
         std::string getValor() const;
 };
 
+/**
+ * Possíveis Valores:\n Agricultura\n Construcao civil\n Energia\n Financas\n Imobiliario\n Papel e celulose\n 
+ * Pecuaria\n Quimica e petroquimica\n Metalurgia e siderurgia\n Mineracao\n
+ */
 //Matricula: 170153169
 class Setor {
     private:
@@ -27,10 +46,23 @@ class Setor {
         void validar(const std::string&);
 
     public:
+        /**
+         * @brief Atualiza o valor do Setor.\n
+         * Novo valor deve pertencer aos valores listados na documentação da classe.
+         * @param valor (string)
+         * @throw Lança invalid_argument caso valor seja inválido.
+         */
         void setValor(const std::string&);
+        /**
+         * @brief Retorna o valor atual do Setor.
+         * @return valor (double)
+         */
         std::string getValor() const;
 };
 
+/**
+ * Dinheiro, em reais. Valor de 0 a 1.000.000,00.
+ */
 //Matricula: 170153169
 class Dinheiro {
     private:
@@ -38,10 +70,23 @@ class Dinheiro {
         void validar(double valor);
 
     public:
+        /**
+         * @brief Retorna o valor atual do Dinheiro.
+         * @return valor (double)
+         */
         double getValor() const;
+        /**
+         * @brief Atualiza o valor do Dinheiro\n
+         * Valor deve ser de 0 a 1.000.000,00
+         * @param valor (double)
+         * @throw Lança invalid_argument caso valor seja inválido.
+         */
         void setValor(double valor);
 };
 
+/**
+ * CPF no formato XXX.XXX.XXX-CC, em que X é dígito de 0-9 e C é dígito de verificação do CPF.
+ */
 //Matricula: 231036050
 class CPF {
     private:
@@ -49,15 +94,27 @@ class CPF {
         void validar(const string&);
 
     public:
+        /**
+         * @brief Retorna o valor atual do CPF.
+         * @return valor (string)
+         */
         const string getValor();
+        /**
+         * @brief Atualiza o valor do CPF.\n
+         * Novo valor deve ser no formato XXX.XXX.XXX-CC. Dígitos de verificação são validados.
+         * @param valor (string)
+         * @throw Lança invalid_argument caso valor seja inválido.
+         */
         void setValor(const string&);
 };
 
-//Matricula: 231036050
 inline const string CPF::getValor() {
     return valor;
 }
 
+/**
+ * Data no formato DD-MM-AAAA, em que DD é dia, MM é mês e AAAA é ano.
+ */
 //Matricula: 231036050
 class Data {
     private:
@@ -65,22 +122,44 @@ class Data {
         void validar(const string&);
 
     public:
+        /**
+         * @brief Retorna o valor atual da Data.
+         * @return valor (string)
+         */
         const string getValor();
+        /**
+         * @brief Atualiza o valor da Data.\n
+         * @param valor (string)
+         * @throw Lança invalid_argument caso valor seja inválido.
+         */
         void setValor(const string&);
 };
 
-//Matricula: 231036050
 inline const string Data::getValor() {
     return valor;
 }
 
+/**
+ * Estado. Possíveis valores:\n
+ * Previsto\n Liquidado\n Inadimplente
+ */
 //Matricula: 231036050
 class Estado {
     private:
         string valor;
         void validar(const string&);
     public:
+        /**
+         * @brief Retorna o valor atual do Estado.
+         * @return valor (string)
+         */
         const string getValor();
+        /**
+         * @brief Atualiza o valor do Estado.\n
+         * Data deve ser no formato DD-MM-AAAA. Ano deve ser de 2000 a 2100. É verificado se a data existe.
+         * @param valor (string)
+         * @throw Lança invalid_argument caso valor seja inválido.
+         */
         void setValor(const string&);
 };
 
